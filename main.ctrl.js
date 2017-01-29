d881d0a459d1eb808650a73fca12cfe12dc5f6d7
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-01-29 06:02:58 +0000

    added new images and updated services section

--- main.ctrl.js
+++ main.ctrl.js
@@ -5,27 +5,16 @@
     service: 'Offensive Skills',
     description: 'The player(s) will learn basic and advanced offensive skills such as fakes, shooting, passing' +
     'and isolation strategies.',
-    pic:'/img/offense.jpg'
+    pic:'/img/offense.png'
   }, {
     service: 'Defensive Skills',
     description: 'The player(s) will learn basic and advanced defense skills such as defensive schemes, spacing,' + 
     'rotations, and more...',
-    pic:'/img/defense.jpg'
+    pic:'/img/defense.png'
   }, {
-    service: 'Ball Handling Skills',
-    description: 'Ball handling skills are essential to all basketball players but a guard needs to espeicially' +
-    'refine these skills through drills and intense practice.',
-    pic:'/img/ballhandling.jpg'
-  }, {
-    service: 'Post Play',
-    description: 'As the game evolves it is becoming more and more esssential for all players to have a balanced' +
-    'offensive game in order to take advantage of mismatches.',
-    pic: '/img/post.jpg'
-  }, {
-    service: 'Group and Indivdual Instruction',
-    description: 'The player(s) will learn basic and advanced offensive skills such as fakes, shooting, passing' +
-    'and isolation strategies',
-    pic: '/img/group.jpg'
-  } 
+    service: 'Conditioning',
+    description: 'Conditioning is imperative in order to keep your play at peak levels throughout a game.',
+    pic:'/img/conditioning.png'
+  }
                 ];
   });
