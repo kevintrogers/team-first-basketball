6940acb4fb87e611429ef76a012e942edd10ae18
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-01-30 18:12:10 +0000

    changed services to svg images, fixed sizing

--- main.ctrl.js
+++ main.ctrl.js
@@ -5,16 +5,16 @@
     service: 'Offensive Skills',
     description: 'The player(s) will learn basic and advanced offensive skills such as fakes, shooting, passing' +
     'and isolation strategies.',
-    pic:'/img/offense.png'
+    pic:'/img/offense.svg'
   }, {
     service: 'Defensive Skills',
     description: 'The player(s) will learn basic and advanced defense skills such as defensive schemes, spacing,' + 
     'rotations, and more...',
-    pic:'/img/defense.png'
+    pic:'/img/defense.svg'
   }, {
     service: 'Conditioning',
     description: 'Conditioning is imperative in order to keep your play at peak levels throughout a game.',
-    pic:'/img/conditioning.png'
+    pic:'/img/conditioning.svg'
   }
                 ];
   });
