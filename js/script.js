4ac093d9bd84d31400899784fc00617cd14dba34
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-02-18 06:33:15 +0000

    reversed non-working modernizr changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  $(function() {
-  
+
   var active, bounceButton = $('.nav-li');
   
   
@@ -12,23 +12,6 @@
 
   });
   
-  if (!Modernizr.svg) {
-  if (Modernizr.canvas) {
-      var canvas = document.createElement("canvas");
-      canvas.setAttribute("style", "height:500px;width:300px;");
-      canvg(canvas, mySVGImage);
-      document.body.appendChild(canvas);
-  } else {
-      // Do something else, perhaps a fallback to PNG?
-  }
-} else {
-  var svgObj = document.createElement('object');
-  svgObj.setAttribute('type', 'image/svg+xml');
-  svgObj.setAttribute('data', 'data:image/svg+xml,' + 'img/team-first-logo.svg');
-  svgObj.setAttribute('height', '500');
-  svgObj.setAttribute('width', '300');
-  document.body.appendChild(svgObj);
-}
 
     
   });
